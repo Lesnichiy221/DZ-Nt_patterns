7073cb467fa0046ef2164f84858ccd8f9bffb6d5
Vlad K <lesnichygpt@gmail.com>
2024-10-25 08:48:52 +0300

    add some more tests

--- src/test/java/ru/netology/ShouldtestCorect.java
+++ src/test/java/ru/netology/ShouldtestCorect.java
@@ -44,12 +44,40 @@ public class ShouldtestCorect {
     @DisplayName("Ошибка при неправильноуказанном городе")
     public void shouldReturnErrorMessageIfCityInvalid() {
 
-        $("[data-test-id='city'] [placeholder='Город']").setValue(DataGenerator.generateCorectCity());
-        $("[data-test-id='name'] [type=text]").setValue(DataGenerator.generateInCorectName());
+        $("[data-test-id='city'] [placeholder='Город']").setValue(DataGenerator.generateInCorectCity());
+        $("[data-test-id='name'] [type=text]").setValue(DataGenerator.generateCorectName());
         $("[data-test-id='phone'] [type=tel]").setValue(DataGenerator.generateCorectPhonenumber());
         $("[data-test-id=agreement]").click();
         $("[role=button] .button__content").click();
         $("[data-test-id='city'].input_invalid .input__sub").shouldBe(visible).
                 shouldHave(exactText("Доставка в выбранный город недоступна"));
+    }
+
+    @Test
+    @DisplayName("Фамилия содержит дефис")
+    void shouldReturnSuccsesfulyIfNameOrSurnameHaveDefise(){
+        $("[data-test-id='city'] [placeholder='Город']").setValue(DataGenerator.generateCorectCity());
+        $("[data-test-id='name'] [type=text]").setValue(DataGenerator.generateCorectName() + "-");
+        $("[data-test-id='phone'] [type=tel]").setValue(DataGenerator.generateCorectPhonenumber());
+        $("[data-test-id=agreement]").click();
+        $("[role=button] .button__content").click();
+        $(withText("Успешно!")).shouldBe(Condition.visible);
+        $("[role=button] .button__content").click();
+        $(withText("Перепланировать")).click();
+    }
+
+    @Test
+    @DisplayName("Нажвание города содержит дефис")
+    void shouldReturnSuccsesfulyIfCityHaveDefise(){
+        $("[data-test-id='city'] [placeholder='Город']").setValue("Ростов-на-дону");
+        $("[data-test-id='name'] [type=text]").setValue(DataGenerator.generateCorectName());
+        $("[data-test-id='phone'] [type=tel]").setValue(DataGenerator.generateCorectPhonenumber());
+        $("[data-test-id=agreement]").click();
+        $("[role=button] .button__content").click();
+        $(withText("Успешно!")).shouldBe(Condition.visible);
+        $("[role=button] .button__content").click();
+        $(withText("Перепланировать")).click();
+
+
     }
 }
